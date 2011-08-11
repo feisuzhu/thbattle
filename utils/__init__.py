df30009c15ec39b1b13b0225d65df14692aadf70
proton <feisuzhu@163.com>
2011-08-11 20:19:06 +0000

    utils: add 'AsyncService'

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,9 @@
+import gevent
+from gevent.queue import Queue
+from gevent.event import AsyncResult
+from gevent import Greenlet
+import types
+
 class PlayerList(list):
     def write(self, data):
         for p in self:
@@ -6,3 +12,19 @@ class PlayerList(list):
     def gwrite(self, data):
         for p in self:
             p.gwrite(data)
+
+class AsyncService(object):
+    
+    def __init__(self):
+        self.request_queue = Queue(10)
+
+    def __getattribute__(self, name):
+        try:
+            f = object.__getattribute__(self, 'service_' + name)
+        except:
+            return Greenlet.__getattribute__(self, name)
+        
+        def asyncservice_wrapper(*args, **kwargs):
+            return gevent.spawn(f, *args, **kwargs)
+
+        return asyncservice_wrapper
