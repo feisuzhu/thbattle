aa7b357706b8e17c062808e6f72fe53297fa4467
proton <feisuzhu@163.com>
2012-01-14 14:51:44 +0800

    UI: fundamental classes
    [Control] and [Overlay] now handles mouse/text events and focus

--- client/ui/base.py
+++ client/ui/base.py
@@ -2,15 +2,15 @@ import pyglet
 
 from pyglet.graphics import *
 from pyglet.gl import *
-from functools import partial
 
 import types
+from time import time
 
 WINDOW_WIDTH = 1000
 WINDOW_HEIGHT = 720
 
 class Control(pyglet.event.EventDispatcher):
-    def __init__(self, x=0, y=0, width=100, height=100, zindex=0, parent=None, can_focus=False):
+    def __init__(self, x=0, y=0, width=100, height=100, zindex=0, parent=None, can_focus=False, *args, **kwargs):
         if parent is None:
             parent = Overlay.cur_overlay
             overlay = parent
@@ -27,7 +27,7 @@ class Control(pyglet.event.EventDispatcher):
             'x': x, 'y': y,
             'width': width, 'height': height,
             'zindex': zindex,
-            'can_focus': can_focus, #TODO
+            'can_focus': can_focus,
         })
         self.control_list = []
         self.continuation = None
@@ -59,6 +59,11 @@ class Control(pyglet.event.EventDispatcher):
         l.reverse()
         return l[0] if l else None
     
+    def stop_drawing(self):
+        if self.continuation:
+            self.continuation.close()
+            self.continuation = None
+    
     def do_draw(self, dt):
         if self.continuation:
             try:
@@ -75,11 +80,15 @@ class Control(pyglet.event.EventDispatcher):
                 '''
                 return self.do_draw(dt)
                 
-        if hasattr(self, 'draw'):
+        elif hasattr(self, 'draw'):
             rst = self.draw(dt)
             if type(rst) == types.GeneratorType:
-                self.continuation = rst
-                rst.next()
+                try:
+                    rst.next()
+                    self.continuation = rst
+                except StopIteration:
+                    print 'Stop first'
+                
         else:
             # default behavior
             if not hasattr(self, 'label'):
@@ -116,25 +125,41 @@ class Control(pyglet.event.EventDispatcher):
             glTranslatef(c.x, c.y, 0)
             c.do_draw(dt)
             glPopMatrix()
-    def on_mouse_enter(self, x, y):
-        print '%s Enter! %d %d' % (self.name, x, y)
-    def on_mouse_leave(self, x, y):
-        print '%s Leave! %d %d' % (self.name, x, y)
+    
+    def set_focus(self):
+        if not self.can_focus: return
+        o = self.parent
+        while not isinstance(o, Overlay):
+            o = o.parent
         
-    def setfocus(self):
-        c = self.parent
-        if c:
-            pass
-            
-        
+        if o:
+            if o.current_focus != self:
+                if o.current_focus:
+                    o.current_focus.dispatch_event('on_lostfocus')
+                self.dispatch_event('on_focus')
+                o.current_focus = self
+
 class Overlay(Control):
     '''
     Represents current screen
     '''
     cur_overlay = None
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         Control.__init__(self, width=WINDOW_WIDTH, height=WINDOW_HEIGHT, parent=False)
         self.__dict__.update(kwargs)
+        self.last_mouse_press = [  # WONTFIX: Button combinations not supported.
+            (0.0, None, 0.0, 0.0), # (time(), self._control_hit, x, y) LEFT
+            (0.0, None, 0.0, 0.0), # MIDDLE
+            None,                  # Not used
+            (0.0, None, 0.0, 0.0), # RIGHT
+        ]
+        self.last_mouse_release = [
+            (0.0, None, 0.0, 0.0), # (time(), self._control_hit, x, y) LEFT
+            (0.0, None, 0.0, 0.0), # MIDDLE
+            None,                  # Not used
+            (0.0, None, 0.0, 0.0), # RIGHT
+        ]
+        self.current_focus = None
     
     def draw(self, dt):
         main_window.clear()
@@ -153,7 +178,7 @@ class Overlay(Control):
         glMatrixMode(GL_MODELVIEW)
         return pyglet.event.EVENT_HANDLED
     
-    def _position_events(self, type, x, y, *args):
+    def _position_events(self, _type, x, y, *args):
         def dispatch(this, lx, ly):
             '''
             # Every hit control get event
@@ -172,7 +197,7 @@ class Overlay(Control):
             
             if not l:
                 if this is not self:
-                    this.dispatch_event(type, lx, ly, *args)
+                    this.dispatch_event(_type, lx, ly, *args)
             else:
                 for c in l:
                     dispatch(c, lx-c.x, ly-c.y)
@@ -189,19 +214,56 @@ class Overlay(Control):
             this._control_hit = c
             
             if not c:
+                # Now 'this' has no subcontrols hit, so 'this' got this event
+                # TODO: if 'this' don't handle it, its parent should handle.
                 if this is not self:
-                    this.dispatch_event(type, lx, ly, *args)
+                    if _type == 'on_mouse_press':
+                        this.set_focus()
+                    this.dispatch_event(_type, lx, ly, *args)
             else:
                 dispatch(c, lx - c.x, ly - c.y) # TODO: not recursive
             
         dispatch(self, x, y)
     
-    on_mouse_motion = lambda self, *args: self._position_events('on_mouse_motion', *args)
-    on_mouse_press = partial(_position_events, 'on_mouse_press')
-    on_mouse_drag = partial(_position_events, 'on_mouse_drag')
-    on_mouse_release = partial(_position_events, 'on_mouse_release')
-    on_mouse_scroll = partial(_position_events, 'on_mouse_scroll')
+    def on_mouse_press(self, x, y, button, modifier):
+        self.last_mouse_press[button] = (time(), self._control_hit, x, y)
+        self._position_events('on_mouse_press', x, y, button, modifier)
+    
+    def on_mouse_release(self, x, y, button, modifier):
+        lp = self.last_mouse_press[button]
+        lr = self.last_mouse_release[button]
+        cr = (time(), self._control_hit, x, y)
+        self.last_mouse_release[button] = cr
+        self._position_events('on_mouse_release', x, y, button, modifier)
+        # single click
+        if cr[1] == lp[1]:
+            self._position_events('on_mouse_click', x, y, button, modifier)
+        
+        # double click
+        if cr[0]-lr[0] < 0.2: # time limit
+            if abs(cr[2] - lr[2]) + abs(cr[3] - lr[3]) < 4: # shift limit
+                if cr[1] == lr[1]: # Control limit
+                    self._position_events('on_mouse_dblclick', x, y, button, modifier)
 
+    on_mouse_motion = lambda self, *args: self._position_events('on_mouse_motion', *args)
+    on_mouse_drag = lambda self, *args: self._position_events('on_mouse_drag', *args)
+    on_mouse_scroll = lambda self, *args: self._position_events('on_mouse_scroll', *args)
+    
+    def _text_events(self, *args):
+        if self.current_focus:
+            self.current_focus.dispatch_event(*args)
+    
+    on_key_press = lambda self, *args: self._text_events('on_key_press', *args)
+    on_key_release = lambda self, *args: self._text_events('on_key_release', *args)
+    on_text = lambda self, *args: self._text_events('on_text', *args)
+    on_text_motion = lambda self, *args: self._text_events('on_text_motion', *args)
+    on_text_motion_select = lambda self, *args: self._text_events('on_text_motion_select', *args)
+
+Control.register_event_type('on_key_press')
+Control.register_event_type('on_key_release')
+Control.register_event_type('on_text')
+Control.register_event_type('on_text_motion')
+Control.register_event_type('on_text_motion_select')
 Control.register_event_type('on_mouse_motion')
 Control.register_event_type('on_mouse_press')
 Control.register_event_type('on_mouse_drag')
@@ -209,9 +271,10 @@ Control.register_event_type('on_mouse_release')
 Control.register_event_type('on_mouse_scroll')
 Control.register_event_type('on_mouse_enter')
 Control.register_event_type('on_mouse_leave')
-
-
-
+Control.register_event_type('on_mouse_click')
+Control.register_event_type('on_mouse_dblclick')
+Control.register_event_type('on_focus')
+Control.register_event_type('on_lostfocus')
 
 def init_gui():
     global main_window
@@ -233,13 +296,13 @@ def init_gui():
         Overlay.cur_overlay.do_draw(dt)
         fps.draw()
         #main_window.flip()
-    pyglet.clock.schedule_interval(_mainwindow_draw, 1/40.0)
+    pyglet.clock.schedule_interval(_mainwindow_draw, 1/60.0)
 
 if __name__ == '__main__':
     init_gui()
-    Control(100, 100).name = 'Foo'
-    Control(150, 150).name = 'Bar'
-    Control(170, 170).name = 'Youmu'
+    Control(100, 100, can_focus=True).name = 'Foo'
+    Control(150, 150, can_focus=True).name = 'Bar'
+    Control(170, 170, can_focus=True).name = 'Youmu'
     '''
     o = Overlay()
     o = o.switch()
@@ -250,7 +313,5 @@ if __name__ == '__main__':
         global o
         o = o.switch()'''
     #pyglet.clock.schedule_interval(sss, 1)
-    
-
     
     pyglet.app.run()
