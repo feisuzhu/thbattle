d3b37b670dda4ffd41b693d9d026323680f2a771
proton <feisuzhu@163.com>
2012-05-08 12:45:15 +0800

    Bug: well, pyglet's bug, workaround.

--- src/client/ui/entry.py
+++ src/client/ui/entry.py
@@ -3,6 +3,7 @@ import threading
 import pyglet
 from client.ui.base import init_gui, schedule as ui_schedule
 from screens import UpdateScreen, ServerSelectScreen
+import sys, os
 
 import logging
 log = logging.getLogger('UI_Entry')
@@ -27,9 +28,13 @@ def start_ui():
         if msg in ('up2date', 'update_disabled', 'error'):
             sss.switch()
         else:
-            import sys, os
             os.execv(sys.executable, [sys.executable] + sys.argv)
 
     Executive.call('update', update_callback, lambda *a: ui_schedule(us.update_message, *a))
 
+
+    # workaround for pyglet's bug
+    if os.platform == 'win32':
+        import pyglet.app.win32
+        pyglet.app.win32.Win32EventLoop._next_idle_time = None
     pyglet.app.run()
