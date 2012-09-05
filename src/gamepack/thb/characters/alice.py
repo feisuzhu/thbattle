a5eb033adb20c8b605ace935abe7986ef73e9bbb
proton <feisuzhu@163.com>
2012-09-05 17:43:46 +0800

    Alice balance tune.

--- src/gamepack/thb/characters/alice.py
+++ src/gamepack/thb/characters/alice.py
@@ -16,8 +16,24 @@ class DollManipulationHandler(EventHandler):
                 act.actor.tags['attack_num'] = 10000
         return act
 
+class DollCrusader(TreatAsSkill):
+    treat_as = DollControlCard
+    def check(self):
+        cl = self.associated_cards
+        if not cl and len(cl) == 1: return False
+        c = cl[0]
+        if c.resides_in.type not in (
+            CardList.HANDCARD,
+            CardList.SHOWNCARD,
+            CardList.EQUIPS,
+        ): return False
+
+        cat = getattr(c, 'equipment_category', None)
+        if cat == 'accessories': return True
+        return False
+
 @register_character
 class Alice(Character):
-    skills = [DollManipulation]
+    skills = [DollManipulation, DollCrusader]
     eventhandlers_required = [DollManipulationHandler]
     maxlife = 4
--- src/gamepack/thb/ui/ui_meta.py
+++ src/gamepack/thb/ui/ui_meta.py
@@ -1990,7 +1990,8 @@ class Alice:
     port_image = gres.alice_port
     description = (
         u'|DB七色的人偶使 爱丽丝 体力：4|r\n\n'
-        u'|G人形操演|r：出牌阶段，你可以使用任意数量的【弹幕】。'
+        u'|G人形操演|r：出牌阶段，你可以使用任意数量的【弹幕】。\n\n'
+        u'|G玩偶十字军|r：出牌阶段，你可以将你的饰品作为【人形操控】使用。'
     )
 
 class DollManipulation:
@@ -2002,6 +2003,48 @@ class DollManipulation:
 
     def is_action_valid(g, cl, target_list):
         return (False, 'BUG!')
+
+class DollCrusader:
+    # Skill
+    name = u'玩偶十字军'
+
+    def clickable(game):
+        me = game.me
+
+        try:
+            act = game.action_stack[-1]
+        except IndexError:
+            return False
+
+        if isinstance(act, actions.ActionStage) and \
+            act.actor is me and \
+            (me.cards or me.showncards or me.equips):
+            return True
+
+        return False
+
+    def is_action_valid(g, cl, target_list):
+        skill = cl[0]
+        cl = skill.associated_cards
+        while True:
+            if len(cl) != 1: break
+            c = cl[0]
+            cat = getattr(c, 'equipment_category', None)
+            if cat != 'accessories': break
+            return cards.DollControlCard.ui_meta.is_action_valid(g, [skill], target_list)
+
+        return (False, u'请选择一张饰品！')
+
+    def effect_string(act):
+        # for LaunchCard.ui_meta.effect_string
+        source = act.source
+        card = act.card
+        target = act.target
+        s = u'|G【%s】|r突然向|G【%s】|r射出魔法丝线，将她当作玩偶一样玩弄了起来！' % (
+            source.ui_meta.char_name,
+            target.ui_meta.char_name,
+        )
+        return s
 
 # ----------
 __metaclass__ = gen_metafunc(characters.nazrin)
