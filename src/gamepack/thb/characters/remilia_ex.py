c48b4afeda1124da8fdc925578d95efc06036cd2
Proton <proton@zhihu.com>
2013-02-21 22:40:34 +0800

    THBRAID: progress #5

--- src/gamepack/thb/characters/remilia_ex.py
+++ src/gamepack/thb/characters/remilia_ex.py
@@ -126,15 +126,118 @@ class NeverNight(Skill):
 
 
 class ScarletFogAction(UserAction):
-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa
+    def apply_action(self):
+        g = Game.getgame()
+        src = self.source
+        tags = src.tags
+        tags['scarletfog_tag'] = tags['turn_count']
+
+        for p in self.target_list:
+            _pl = g.attackers[:]
+            _pl.remove(p)
+            pl = []
+            atkcard = AttackCard()
+            for t in _pl:
+                if LaunchCard(p, [t], atkcard).can_fire():
+                    pl.append(t)
+
+            rst = user_choose_cards_and_players(self, p, [p.cards, p.showncards], pl)
+            if rst:
+                c = rst[0][0]; t = rst[1][0]
+                g.process_action(LaunchCard(p, t, c))
+            else:
+                g.process_action(LifeLost(p, p, 1))
+
+        return True
+
+    def cond(self, cl):
+        return len(cl) == 1 and cl[0].is_card(AttackCard)
+
+    def choose_player_target(self, tl):
+        if not tl:
+            return (tl, False)
+
+        return (tl[-1:], True)
+
+    def is_valid(self):
+        tags = self.source.tags
+        return tags['turn_count'] > tags['scarletfog_tag']
+
 
 class ScarletFog(Skill):
+    associated_action = ScarletFogAction
+    target = t_All
+    def check(self):
+        cl = self.associated_cards
+        if not len(cl) == 1: return False
+        c = cl[0]
+        if c.is_card(VirtualCard): return False
+        if c.color != Card.RED: return False
+        return True
+
+
+class QueenOfMidnight(Skill):
+    associated_action = None
+    target = t_None
+
+
+class QueenOfMidnightHandler(EventHandler):
+    def handle(self, evt_type, act):
+        if evt_type == 'action_before' and isinstance(act, ActionStage):
+            g = Game.getgame()
+            tgt = act.target
+            if not tgt.has_skill(QueenOfMidnight): return act
+            g.process_action(DrawCards(act.target, 2))
+
+        elif evt_type == 'action_before' and isinstance(act, DropCardStage):
+            tgt = act.target
+            if tgt.has_skill(QueenOfMidnight):
+                act.dropn = max(act.dropn - 2, 0)
+
+        return act
+
+
+class Septet(Skill):
+    associated_action = None
+    target = t_None
+
+
+class SeptetHandler(EventHandler):
+    def handle(self, evt_type, act):
+        if evt_type == 'action_after' and isinstance(act, DelayedSpellCardAction):
+            src = act.source
+            tgt = act.target
+            if not src.has_skill(Septet): return act
+            self.action = act
+            c = user_choose_cards(self, src, [src.cards, src.showncards])
+            g = Game.getgame()
+            if c:
+                g.process_action(DropCards(src, [c]))
+            else:
+                g.process_action(DropCards(tgt, [act.card]))
+
+        return act
+
 
 class RemiliaEx2(Character):
     maxlife = 6
     maxfaith = 4
-    skills = [HeartBreak, NeverNight, VampireKiss, FateSpear, ScarletFog]
-    eventhandlers_required = [FateSpearHandler, VampireKissHandler]
+    skills = [
+        HeartBreak,
+        NeverNight,
+        VampireKiss,
+        FateSpear,
+        ScarletFog,
+        QueenOfMidnight,
+        Septet,
+    ]
+
+    eventhandlers_required = [
+        FateSpearHandler,
+        VampireKissHandler,
+        QueenOfMidnightHandler,
+        SeptetHandler,
+    ]
 
     initial_equips = [(GungnirCard, SPADE, Q)]
 
