4b42c0889d5bf344c6c7b647d58022343583c32e
Proton <proton@zhihu.com>
2013-07-23 23:51:16 +0800

    incr ver, welcome msg.

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ else:
 if sys.platform.startswith('linux'):
     UPDATE_URL += 'src/'
 
-VERSION = 'THBATTLE V1.0b incr 143'
+VERSION = 'THBATTLE V1.0b incr 144'
 
 HALL_NOTICE_URL = 'http://www.thbattle.net/notice.txt'
 
