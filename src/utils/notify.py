b6a54f4afee1821a8a5d293fff8f6b6a95665abf
Proton <proton@zhihu.com>
2013-08-11 19:17:54 +0800

    notify.py format tune

--- src/utils/notify.py
+++ src/utils/notify.py
@@ -1,24 +1,22 @@
-def _notify(title, msg):
-	pass
-
+# -*- coding: utf-8 -*-
 NONE = 0
 BASIC = 1
 SPEAKER = 2
 
-def notify(title, msg, level = BASIC):
-    from user_settings import UserSettings as us
-    if level <= us.notify_level:
-        _notify(title, msg)
 
-__all__ = ['notify']
+def _notify(title, msg):
+    pass
+
 
 try:
     import pynotify
     pynotify.init('thbattle')
-    n = pynotify.Notification('None')
-    def _notify(title, msg):
+
+    def _notify(title, msg):  # noqa
+        n = pynotify.Notification('None')
         n.update(title, msg)
         n.show()
+
 except ImportError:
     pass
 
@@ -26,22 +24,25 @@ import os
 import platform
 
 if os.name == 'nt' or platform.system() == 'Windows':
-    from ctypes import *
-    from ctypes.wintypes import *
+    from ctypes import Structure, windll, sizeof, byref
+    from ctypes.wintypes import DWORD, HICON, HWND, UINT, WCHAR
+
     class NOTIFYICONDATAW(Structure):
-        _fields_ = [("cbSize", DWORD),
-                    ("hWnd", HWND),
-                    ("uID", UINT),
-                    ("uFlags", UINT),
-                    ("uCallbackMessage", UINT),
-                    ("hIcon", HICON),
-                    ("szTip", WCHAR * 128),
-                    ("dwState", DWORD),
-                    ("dwStateMask", DWORD),
-                    ("szInfo", WCHAR * 256),
-                    ("uVersion", UINT),
-                    ("szInfoTitle", WCHAR * 64),
-                    ("dwInfoFlags", DWORD)]
+        _fields_ = [
+            ("cbSize", DWORD),
+            ("hWnd", HWND),
+            ("uID", UINT),
+            ("uFlags", UINT),
+            ("uCallbackMessage", UINT),
+            ("hIcon", HICON),
+            ("szTip", WCHAR * 128),
+            ("dwState", DWORD),
+            ("dwStateMask", DWORD),
+            ("szInfo", WCHAR * 256),
+            ("uVersion", UINT),
+            ("szInfoTitle", WCHAR * 64),
+            ("dwInfoFlags", DWORD),
+        ]
 
     NIM_ADD = 0
     NIM_MODIFY = 1
@@ -64,8 +65,8 @@ if os.name == 'nt' or platform.system() == 'Windows':
     GCLP_HICON = -14
 
     has_init = False
-    
-    def _notify(title, msg):
+
+    def _notify(title, msg):  # noqa
         global data, has_init
 
         if not has_init:
@@ -81,7 +82,7 @@ if os.name == 'nt' or platform.system() == 'Windows':
             data.uVersion = 3
             windll.shell32.Shell_NotifyIconW(NIM_ADD, byref(data))
             windll.shell32.Shell_NotifyIconW(NIM_SETVERSION, byref(data))
-            data.uVersion = 10 # uTimeout
+            data.uVersion = 10  # uTimeout
             data.uFlags = NIF_INFO
             data.dwInfoFlags = NIIF_INFO
 
@@ -93,3 +94,12 @@ if os.name == 'nt' or platform.system() == 'Windows':
         data.szInfoTitle = unicode(title)[:63]
         data.szInfo = unicode(msg)[:255]
         windll.shell32.Shell_NotifyIconW(NIM_MODIFY, byref(data))
+
+
+def notify(title, msg, level=BASIC):
+    from user_settings import UserSettings as us
+    if level <= us.notify_level:
+        _notify(title, msg)
+
+
+__all__ = ['notify', 'NONE', 'BASIC', 'SPEAKER']
