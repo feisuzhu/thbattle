f3e0d927adbf09e1e4d6e2b41f2817af46c1865b
zhyk <zhykzhykzhyk@gmail.com>
2014-06-20 01:24:27 +0800

    incr ver, welcome msg.

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,7 @@ else:
 if sys.platform.startswith('linux'):
     UPDATE_URL += 'src/'
 
-VERSION = 'THBATTLE V1.0b incr 202'
+VERSION = 'THBATTLE V1.0b incr 203'
 
 HALL_NOTICE_URL = 'http://www.thbattle.net/notice.txt'
 
@@ -151,6 +151,7 @@ Proton制作
 http://www.thbattle.net
 
 |B最近更新情况：|r
+新人物：射命丸文（画师：渚FUN）、SP八云紫（画师：Vivicat From 东方梦斗符）（开发：zhyk）
 修复了与八云紫移动伊吹瓢相关的bug
 天子、梅蒂欣、妹红、正邪、橙：技能、设定调整
 妖梦技能调整：觉醒技效果并入二刀流
