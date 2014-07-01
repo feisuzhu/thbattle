9d112ebb04bc2753df27a8b0bea295b277a7622b
zhyk <zhykzhykzhyk@gmail.com>
2014-07-01 20:02:53 +0800

    SpYukari: Add FAQ

--- src/gamepack/thb/ui/ui_meta/characters/sp_yukari.py
+++ src/gamepack/thb/ui/ui_meta/characters/sp_yukari.py
@@ -65,6 +65,7 @@ class SpYukari:
     figure_image = gres.sp_yukari_figure
     description = (
         u'|DB神隐的主犯 八云紫 体力：4|r\n\n'
-        u'|G神隐|r：出牌阶段限两次，你可以将任意角色区域内的一张牌移出游戏。你的回合结束阶段，那名玩家获得那张牌。\n\n'
+        u'|G神隐|r：出牌阶段限两次，你可以将任意角色区域内的一张牌移出游戏。你的回合结束阶段，那名玩家获得那张牌。\n'
+        u'|B|R>> |r你可以观看由|G神隐|r移出的牌。\n\n'
         u'|DB（画师：Vivicat from 幻想梦斗符）|r'
     )
