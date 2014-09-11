fbfcefed97ba69df41e0dc553519008a784f4424
Proton <feisuzhu@163.com>
2014-09-11 15:24:15 +0800

    incr ver, welcome msg

--- src/settings.py
+++ src/settings.py
@@ -24,7 +24,7 @@ UPDATE_PREFIX = 'src/' if sys.platform.startswith('linux') else ''
 
 from options import options
 
-VERSION = 'THBATTLE V1.0b incr 218'
+VERSION = 'THBATTLE V1.0b incr 219'
 
 HALL_NOTICE_URL = 'http://www.thbattle.net/notice.txt'
 
