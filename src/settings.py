a463198ef725f5b5a9ac7d6ecbcd33ab62dfb155
Proton <feisuzhu@163.com>
2014-12-29 21:21:29 +0800

    incr ver, welcome msg.

--- src/settings.py
+++ src/settings.py
@@ -8,7 +8,7 @@ __metaclass__ = lambda _1, _2, _dict: _dict.pop('__module__', '..') and _dict
 
 
 # -- begin settings --
-VERSION = 'THBATTLE V1.0b incr 237'
+VERSION = 'THBATTLE V1.0b incr 238'
 
 CWD = os.getcwd()
 BASEDIR = os.path.dirname(os.path.realpath(__file__))
