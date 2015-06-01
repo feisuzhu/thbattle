dd2d3dff2ccc67337f99dd701ef643c31a03ea47
zhyk <zhykzhykzhyk@gmail.com>
2015-06-01 20:17:26 +0800

    notify cocoa: bring window to front

--- src/utils/notify/cocoa.py
+++ src/utils/notify/cocoa.py
@@ -1,10 +1,26 @@
-from pyglet.libs.darwin.cocoapy import ObjCClass, get_NSString
+from pyglet.libs.darwin.cocoapy import ObjCClass, get_NSString, objc, ObjCSubclass
 
 NSUserNotificationCenter = ObjCClass("NSUserNotificationCenter")
 NSUserNotification = ObjCClass("NSUserNotification")
+NSApplication = ObjCClass("NSApplication")
+NSUserNotificationCenterDelegate = objc.objc_getProtocol(bytes("NSUserNotificationCenterDelegate"))
+
+NSApp = NSApplication.sharedApplication()
+
+NotificationDelegateClass = ObjCSubclass('NSObject', 'NotificationDelegate', register=False)
+@NotificationDelegateClass.method("v@@")
+def userNotificationCenter_didActivateNotification_(self, notificationCenter, notification):
+    # bring window to front
+    NSApp.activateIgnoringOtherApps_(True)
+
+objc.class_addProtocol(NotificationDelegateClass.objc_cls, NSUserNotificationCenterDelegate)
+NotificationDelegateClass.register()
+NotificationDelegate = ObjCClass("NotificationDelegate")
+
+notificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
+notificationCenter.setDelegate_(NotificationDelegate.alloc().init().autorelease())
 
 def _notify(title, msg):
-    notificationCenter = NSUserNotificationCenter.defaultUserNotificationCenter()
     notification = NSUserNotification.alloc().init().autorelease()
     notification.setTitle_(get_NSString(title))
     notification.setInformativeText_(get_NSString(msg))
