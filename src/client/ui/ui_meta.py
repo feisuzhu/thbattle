753384f2526e7eca815c38afcfbf81e23875ca63
Proton <feisuzhu@163.com>
2015-08-10 21:41:20 +0800

    Badges update

--- src/client/ui/ui_meta.py
+++ src/client/ui/ui_meta.py
@@ -24,39 +24,9 @@ class jcb_1_gold:
     badge_text = u'第一届“节操杯”符斗祭比赛冠军队'
 
 
-class jcb_2_gold:
-    badge_anim = 'c-badges-jcb_gold'
-    badge_text = u'第二届“节操杯”符斗祭比赛冠军队'
-
-
-class jcb_3_gold:
-    badge_anim = 'c-badges-jcb_gold'
-    badge_text = u'第三届“节操杯”符斗祭比赛冠军队'
-
-
-class jcb_4_gold:
-    badge_anim = 'c-badges-jcb_gold'
-    badge_text = u'第四届“节操杯”符斗祭比赛冠军队'
-
-
 class jcb_1_silver:
     badge_anim = 'c-badges-jcb_silver'
     badge_text = u'第一届“节操杯”符斗祭比赛亚军队'
-
-
-class jcb_2_silver:
-    badge_anim = 'c-badges-jcb_silver'
-    badge_text = u'第二届“节操杯”符斗祭比赛亚军队'
-
-
-class jcb_3_silver:
-    badge_anim = 'c-badges-jcb_silver'
-    badge_text = u'第三届“节操杯”符斗祭比赛亚军队'
-
-
-class jcb_4_silver:
-    badge_anim = 'c-badges-jcb_silver'
-    badge_text = u'第四届“节操杯”符斗祭比赛亚军队'
 
 
 class jcb_1_bronze:
@@ -64,9 +34,29 @@ class jcb_1_bronze:
     badge_text = u'第一届“节操杯”符斗祭比赛季军队'
 
 
+class jcb_2_gold:
+    badge_anim = 'c-badges-jcb_gold'
+    badge_text = u'第二届“节操杯”符斗祭比赛冠军队'
+
+
+class jcb_2_silver:
+    badge_anim = 'c-badges-jcb_silver'
+    badge_text = u'第二届“节操杯”符斗祭比赛亚军队'
+
+
 class jcb_2_bronze:
     badge_anim = 'c-badges-jcb_bronze'
     badge_text = u'第二届“节操杯”符斗祭比赛季军队'
+
+
+class jcb_3_gold:
+    badge_anim = 'c-badges-jcb_gold'
+    badge_text = u'第三届“节操杯”符斗祭比赛冠军队'
+
+
+class jcb_3_silver:
+    badge_anim = 'c-badges-jcb_silver'
+    badge_text = u'第三届“节操杯”符斗祭比赛亚军队'
 
 
 class jcb_3_bronze:
@@ -74,9 +64,34 @@ class jcb_3_bronze:
     badge_text = u'第三届“节操杯”符斗祭比赛季军队'
 
 
+class jcb_4_gold:
+    badge_anim = 'c-badges-jcb_gold'
+    badge_text = u'第四届“节操杯”符斗祭比赛冠军队'
+
+
+class jcb_4_silver:
+    badge_anim = 'c-badges-jcb_silver'
+    badge_text = u'第四届“节操杯”符斗祭比赛亚军队'
+
+
 class jcb_4_bronze:
     badge_anim = 'c-badges-jcb_bronze'
     badge_text = u'第四届“节操杯”符斗祭比赛季军队'
+
+
+class jcb_5_gold:
+    badge_anim = 'c-badges-jcb_gold'
+    badge_text = u'第五届“节操杯”符斗祭比赛冠军队'
+
+
+class jcb_5_silver:
+    badge_anim = 'c-badges-jcb_silver'
+    badge_text = u'第五届“节操杯”符斗祭比赛亚军队'
+
+
+class jcb_5_bronze:
+    badge_anim = 'c-badges-jcb_bronze'
+    badge_text = u'第五届“节操杯”符斗祭比赛季军队'
 
 
 class dsb_1_gold:
@@ -84,19 +99,9 @@ class dsb_1_gold:
     badge_text = u'第一届大师杯冠军'
 
 
-class dsb_2_gold:
-    badge_anim = 'c-badges-dsb_gold'
-    badge_text = u'第二届大师杯冠军'
-
-
 class dsb_1_silver:
     badge_anim = 'c-badges-dsb_silver'
     badge_text = u'第一届大师杯亚军'
-
-
-class dsb_2_silver:
-    badge_anim = 'c-badges-dsb_silver'
-    badge_text = u'第二届大师杯亚军'
 
 
 class dsb_1_bronze:
@@ -104,9 +109,49 @@ class dsb_1_bronze:
     badge_text = u'第一届大师杯季军'
 
 
+class dsb_2_gold:
+    badge_anim = 'c-badges-dsb_gold'
+    badge_text = u'第二届大师杯冠军'
+
+
+class dsb_2_silver:
+    badge_anim = 'c-badges-dsb_silver'
+    badge_text = u'第二届大师杯亚军'
+
+
 class dsb_2_bronze:
     badge_anim = 'c-badges-dsb_bronze'
     badge_text = u'第二届大师杯季军'
+
+
+class fff_1_gold:
+    badge_anim = 'c-badges-fff_gold'
+    badge_text = u'第一届“FFF杯”符斗祭比赛冠军队'
+
+
+class fff_1_silver:
+    badge_anim = 'c-badges-fff_silver'
+    badge_text = u'第一届“FFF杯”符斗祭比赛亚军队'
+
+
+class fff_2_bronze:
+    badge_anim = 'c-badges-fff_bronze'
+    badge_text = u'第二届“FFF杯”符斗祭比赛季军队'
+
+
+class fff_2_gold:
+    badge_anim = 'c-badges-fff_gold'
+    badge_text = u'第二届“FFF杯”符斗祭比赛冠军队'
+
+
+class fff_2_silver:
+    badge_anim = 'c-badges-fff_silver'
+    badge_text = u'第二届“FFF杯”符斗祭比赛亚军队'
+
+
+class fff_1_bronze:
+    badge_anim = 'c-badges-fff_bronze'
+    badge_text = u'第二届“FFF杯”符斗祭比赛季军队'
 
 
 class contributor:
@@ -116,10 +161,5 @@ class contributor:
         u'在官方发起的公开任务中积极参与并做出贡献的玩家'
     )
 
-
-'''
-badge_text = u'大师の期末考状元'
-badge_text = u'大师の考题状元'
-'''
 
 # -----END BADGES UI META-----
