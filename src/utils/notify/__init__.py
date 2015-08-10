761f123485b4e9a9730f05e9bf9bc8b36abb6b63
Proton <feisuzhu@163.com>
2015-08-11 00:20:38 +0800

    utils.notify lazy init

--- src/utils/notify/__init__.py
+++ src/utils/notify/__init__.py
@@ -1,38 +1,46 @@
 # -*- coding: utf-8 -*-
+
 NONE = 0
 BASIC = 0.5
 AT = 1
 SPEAKER = 2
 
 
-def _notify(title, msg):
-    pass
+def _get_notify():
+    # lazy init, mobile platforms needs this
 
-import os
-import platform
-import sys
+    import os
+    import platform
+    import sys
 
-if os.name == 'nt' or platform.system() == 'Windows':
-    from .win32 import _notify
+    if os.name == 'nt' or platform.system() == 'Windows':
+        from .win32 import _notify
+        return _notify
 
-elif sys.platform == 'darwin':
-    from .cocoa import _notify
+    elif sys.platform == 'darwin':
+        from .cocoa import _notify
+        return _notify
 
-else:
-    try:
-        import pynotify
-    except ImportError:
-        pass
     else:
-        from .pynotify_adapter import _notify
+        try:
+            import pynotify  # noqa
+        except ImportError:
+            pass
+        else:
+            from .pynotify_adapter import _notify
+            return _notify
+
+    return lambda *a, **k: None
+
 
 def notify(title, msg, level=BASIC):
     from user_settings import UserSettings as us
     if level <= us.notify_level:
-        _notify(title, msg)
+        _get_notify()(title, msg)
         if us.sound_notify:
             from client.ui.soundmgr import SoundManager
 
             SoundManager.play('c-sound-input')
+
 
 __all__ = ['notify', 'NONE', 'BASIC', 'SPEAKER', 'AT']
