3905b454f41204478e9dd65c87f0a084d67d85ea
zhyk <zhykzhykzhyk@gmail.com>
2015-09-03 17:34:11 +0800

    Medicine tune.

--- src/gamepack/thb/characters/medicine.py
+++ src/gamepack/thb/characters/medicine.py
@@ -3,9 +3,9 @@
 # -- stdlib --
 # -- third party --
 # -- own --
-from ..actions import Damage, DrawCards, DropCards, FatetellStage, GenericAction, PlayerTurn
+from ..actions import Damage, DrawCards, DropCards, FatetellStage, GenericAction, LifeLost
 from ..actions import ShowCards, UserAction, user_choose_cards, LaunchCard, UseCard
-from ..cards import Card, Skill, SoberUp, VirtualCard, Wine, t_None
+from ..cards import Card, Skill, VirtualCard, Wine, t_None
 from ..inputlets import ChooseOptionInputlet
 from .baseclasses import Character, register_character
 from game.autoenv import EventHandler, Game, user_input, ActionShootdown
@@ -29,46 +29,10 @@ class CiguateraAction(UserAction):
         src = self.source
         g = Game.getgame()
         g.process_action(DropCards(src, src, self.cards))
-        g.process_action(Wine(tgt, tgt))
-        tags = tgt.tags
-        tags['ciguatera_tag'] = g.turn_count
-        tags['ciguatera_src'] = src
+        g.process_action(LifeLost(src, tgt, 1))
+        g.process_action(Wine(src, tgt))
 
         return True
-
-
-class CiguateraTurnEnd(GenericAction):
-    card_usage = 'drop'
-
-    def apply_action(self):
-        src = self.source
-        tgt = self.target
-        g = Game.getgame()
-        g.process_action(SoberUp(src, src))
-
-        draw = DrawCards(tgt, amount=1)
-
-        if draw.can_fire():
-            cards = user_choose_cards(self, src, ('cards', 'showncards'))
-        else:
-            cards = None
-
-        if cards:
-            assert len(cards) == 1
-            self.card = cards[0]
-            g.process_action(DropCards(src, src, cards))
-            g.process_action(draw)
-        else:
-            self.card = None
-            g.process_action(Damage(None, src))
-
-        return True
-
-    def cond(self, cl):
-        return len(cl) == 1 and not cl[0].is_card(Skill)
-
-    def is_valid(self):
-        return self.source.tags.get('wine', False)
 
 
 class CiguateraHandler(EventHandler):
@@ -79,20 +43,15 @@ class CiguateraHandler(EventHandler):
         if evt_type == 'action_before' and isinstance(act, FatetellStage):
             g = Game.getgame()
             for p in g.players:
-                if p.dead: continue
-                if not p.has_skill(Ciguatera): continue
+                if p.dead:
+                    continue
+
+                if not p.has_skill(Ciguatera):
+                    continue
 
                 cards = user_choose_cards(self, p, ('cards', 'showncards'))
                 if cards:
                     g.process_action(CiguateraAction(p, act.target, cards))
-
-        if evt_type == 'action_after' and isinstance(act, PlayerTurn):
-            tgt = act.target
-            tags = tgt.tags
-            g = Game.getgame()
-            if tags.get('ciguatera_tag') == g.turn_count:
-                src = tgt.tags['ciguatera_src']
-                g.process_action(CiguateraTurnEnd(tgt, src))
 
         return act
 
@@ -150,13 +109,16 @@ class MelancholyHandler(EventHandler):
             tgt = act.target
             src = act.source
 
-            if not src: return act
-            if not tgt.has_skill(Melancholy): return act
+            if not src:
+                return act
+
+            if not tgt.has_skill(Melancholy):
+                return act
 
             if not user_input([tgt], ChooseOptionInputlet(self, (False, True))):
                 return act
 
-            Game.getgame().process_action(MelancholyAction(tgt, src, amount=act.amount))
+            Game.getgame().process_action(MelancholyAction(tgt, src, amount=1))
 
         elif evt_type == 'action_shootdown' and isinstance(act, (LaunchCard, UseCard)):
             src = act.source
