acb46df59a1f801acf6a92b011fa8e8d5d7a02ae
zhyk <zhykzhykzhyk@gmail.com>
2015-09-12 22:19:03 +0800

    SpAya tune.

--- src/gamepack/thb/characters/sp_aya.py
+++ src/gamepack/thb/characters/sp_aya.py
@@ -5,19 +5,13 @@
 # -- own --
 from game.autoenv import EventHandler, Game, InputTransaction, user_input
 from gamepack.thb.actions import ActionStage, ActionStageLaunchCard, DrawCards, DropCardStage
-from gamepack.thb.actions import GenericAction, PlayerTurn, UserAction, ask_for_action, ttags
-from gamepack.thb.cards import AttackCard, Skill, VirtualCard, t_None
+from gamepack.thb.actions import GenericAction, PlayerTurn, UserAction, ask_for_action, LaunchCard
+from gamepack.thb.cards import AttackCard, Skill, VirtualCard, t_Self, t_None, PhysicalCard, Card
 from gamepack.thb.characters.baseclasses import Character, register_character
 from gamepack.thb.inputlets import ChooseOptionInputlet
 
 
 # -- code --
-class WindWalk(Skill):
-    associated_action = None
-    skill_category = ('character', 'passive')
-    target = t_None
-
-
 class WindWalkLaunch(ActionStageLaunchCard):
     pass
 
@@ -41,27 +35,27 @@ class WindWalkAction(UserAction):
     def apply_action(self):
         g = Game.getgame()
         tgt = self.target
-        dc = DrawCards(tgt, 1)
-        g.process_action(dc)
-        c, = dc.cards
-        self.card = c
 
-        ttags(tgt)['aya_windwalk'] = True
+        while True:
+            dc = DrawCards(tgt, 1)
+            g.process_action(dc)
+            c, = dc.cards
+            self.card = c
 
-        with InputTransaction('ActionStageAction', [tgt]) as trans:
-            p, rst = ask_for_action(
-                self, [tgt], ('cards', 'showncards'), g.players, trans
-            )
+            with InputTransaction('ActionStageAction', [tgt]) as trans:
+                p, rst = ask_for_action(
+                    self, [tgt], ('cards', 'showncards'), g.players, trans
+                )
 
-        if p is not tgt:
-            g.process_action(WindWalkSkipAction(tgt, tgt))
-            return True
+            if p is not tgt:
+                g.process_action(WindWalkSkipAction(tgt, tgt))
+                break
 
-        cl, tl = rst
-        g.players.reveal(cl)
-        c, = cl
+            cl, tl = rst
+            g.players.reveal(cl)
+            c, = cl
 
-        g.process_action(WindWalkLaunch(p, tl, c))
+            g.process_action(WindWalkLaunch(p, tl, c))
 
         return True
 
@@ -73,7 +67,9 @@ class WindWalkAction(UserAction):
         ])
 
     def cond(self, cl):
-        if not (cl and len(cl) == 1): return False
+        if not (cl and len(cl) == 1):
+            return False
+
         return bool(cl[0].associated_action) and [self.card] == VirtualCard.unwrap(cl)
 
     def ask_for_action_verify(self, p, cl, tl):
@@ -84,36 +80,69 @@ class WindWalkAction(UserAction):
         return tl, True
 
 
-class WindWalkHandler(EventHandler):
-    interested = ('action_after', )
+class WindWalk(Skill):
+    associated_action = WindWalkAction
+    skill_category = ('character', 'active')
+    target = t_Self
+    usage = 'drop'
+
+    def check(self):
+        cl = self.associated_cards
+        if not(cl and len(cl) == 1):
+            return False
+
+        if cl[0].is_card(Skill):
+            return False
+
+        return True
+
+
+class Dominance(Skill):
+    associated_action = None
+    skill_category = ('character', 'passive')
+    target = t_None
+
+
+class DominanceHandler(EventHandler):
+    interested = ('action_after', 'action_apply')
 
     def handle(self, evt_type, act):
+        if evt_type == 'action_apply' and isinstance(act, PlayerTurn):
+            act.target.tags['dominance_suits'] = set()
 
-        if evt_type == 'action_after' and isinstance(act, ActionStageLaunchCard):
-            src = act.source
-
-            if not src.has_skill(WindWalk):
+        elif evt_type == 'action_apply' and isinstance(act, LaunchCard):
+            card = act.card
+            if not card.is_card(PhysicalCard):
                 return act
 
-            c = act.card
-
-            if not all([
-                WindWalkAction.is_windwalk_card(c),
-                act.card_action.succeeded,
-                isinstance(act, WindWalkLaunch) or not ttags(src)['aya_windwalk'],
-            ]): return act
-
-            if not user_input([src], ChooseOptionInputlet(self, (False, True))):
+            suit = card.suit
+            if suit not in (Card.SPADE, Card.CLUB, Card.HEART, Card.DIAMOND):
                 return act
 
-            g = Game.getgame()
-            g.process_action(WindWalkAction(src, src))
+            try:
+                act.source.tags['dominance_suits'].add(suit)
+
+            except AttributeError:
+                pass
+
+        elif evt_type == 'action_after' and isinstance(act, PlayerTurn):
+            tgt = act.target
+            if not tgt.has_skill(Dominance):
+                return act
+
+            if len(tgt.tags['dominance_suits']) != 4:
+                return act
+
+            if not user_input([tgt], ChooseOptionInputlet(self, (False, True))):
+                return act
+
+            Game.getgame().process_action(PlayerTurn(tgt))
 
         return act
 
 
 @register_character
 class SpAya(Character):
-    skills = [WindWalk]
-    eventhandlers_required = [WindWalkHandler]
+    skills = [WindWalk, Dominance]
+    eventhandlers_required = [DominanceHandler]
     maxlife = 4
--- src/gamepack/thb/ui/ui_meta/characters/sp_aya.py
+++ src/gamepack/thb/ui/ui_meta/characters/sp_aya.py
@@ -3,8 +3,8 @@
 # -- stdlib --
 # -- third party --
 # -- own --
-from gamepack.thb import characters
-from gamepack.thb.ui.ui_meta.common import gen_metafunc, passive_clickable, passive_is_action_valid
+from gamepack.thb import characters, cards
+from gamepack.thb.ui.ui_meta.common import gen_metafunc, passive_clickable, passive_is_action_valid, my_turn
 
 
 # -- code --
@@ -13,26 +13,41 @@ __metaclass__ = gen_metafunc(characters.sp_aya)
 
 class WindWalk:
     # Skill
-    name = u'疾风步'
-    clickable = passive_clickable
-    is_action_valid = passive_is_action_valid
+    name = u'疾走'
+
+    def clickable(g):
+        if not my_turn():
+            return False
+
+        me = g.me
+        return bool(me.cards or me.showncards or me.equips)
+
+    def is_action_valid(g, cl, tl):
+        acards = cl[0].associated_cards
+        if (not acards) or len(acards) != 1:
+            return (False, u'请选择一张牌')
+
+        card = acards[0]
+
+        if card.resides_in.type not in ('cards', 'showncards', 'equips'):
+            return (False, u'请选择一张牌!')
+
+        if card.is_card(cards.Skill):
+            return (False, u'你不可以像这样组合技能')
+
+        return (True, u'疾走')
 
 
 class WindWalkLaunch:
     pass
 
 
-class WindWalkHandler:
-    choose_option_prompt = u'你要发动【疾风步】吗？'
-    choose_option_buttons = ((u'发动', True), (u'不发动', False))
-
-
 class WindWalkAction:
-    idle_prompt = u'疾风步：请使用摸到的牌（否则结束出牌并跳过弃牌阶段）'
+    idle_prompt = u'疾走：请使用摸到的牌（否则结束出牌并跳过弃牌阶段）'
 
     def choose_card_text(g, act, cards):
         if not act.cond(cards):
-            return False, u'疾风步：只能使用摸到的牌（或者结束）'
+            return False, u'疾走：只能使用摸到的牌（或者结束）'
         else:
             return True, u'不会显示……'
 
@@ -45,6 +60,18 @@ class WindWalkSkipAction:
         return u'|G【%s】|r放弃了追击。' % act.target.ui_meta.char_name
 
 
+class DominanceHandler:
+    choose_option_prompt = u'你要发动【风靡】吗？'
+    choose_option_buttons = ((u'发动', True), (u'不发动', False))
+
+
+class Dominance:
+    # Skill
+    name = u'风靡'
+    clickable = passive_clickable
+    is_action_valid = passive_is_action_valid
+
+
 class SpAya:
     # Character
     char_name = u'SP射命丸文'
@@ -52,9 +79,14 @@ class SpAya:
     figure_image = 'thb-figure-sp_aya'
     description = (
         u'|DB剑圣是谁有我快吗 SP射命丸文 体力：4|r\n\n'
-        u'|G疾风步|r：出牌阶段限一次，当你使用的一张|G弹幕|r或非群体非延时符卡生效并结算完毕后，你可以摸一张牌并选择：\n'
-        u'|B|R>> |r立即使用此牌，且若此牌满足|G疾风步|r的发动条件，你可以再次发动|G疾风步|r\n'
-        u'|B|R>> |r结束出牌阶段，并跳过弃牌阶段\n'
+        u'|G疾走|r：出牌阶段，你可弃置一张牌；若你如此做，你亮出牌堆顶的一张牌，并选择：1.立刻使用之，然后重复此流程。2.获得此牌，并结束此回合。\n'
+        # 比你快！
+        # 还是放一下水吧
+        u'\n'
+        u'|G风靡|r：回合结束时，若你本回合的出牌阶段使用了四种花色的牌，你可执行一个额外的回合。\n'
+        #  好像有大事件，还不能休息！
         u'\n'
         u'|DB（人物设计：吹风姬，画师：躲猫）|r'
+        # 尽管放了水，不过你真的好厉害啊
+
     )
