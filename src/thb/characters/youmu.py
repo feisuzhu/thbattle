43d8ef1601c3d3194cd880fa6b78a7e134088107
CoffeeYin-CY <1270821450@qq.com>
2019-11-13 00:22:37 +0800

    Changes on Adjustment of Youmu's Vatality: If in ActionStage she puts on new UFO or suit which drops non-weapon equip, she will not lose necessary vitality.

--- src/thb/characters/youmu.py
+++ src/thb/characters/youmu.py
@@ -109,7 +109,7 @@ class YoumuHandler(EventHandler):
                 n = self.weapons(p)
                 dn = len(cards)
 
-                if cl is _from and (dn + n) >= 2 and n <= 1:
+                if cl is _from and (dn + n) >= 2 and n <= 1 and any(getattr(c, 'equipment_category', None) == 'weapon' for c in cards):
                     adjust = -1
                 elif cl is to and (n - dn) <= 1 and n >= 2:
                     adjust = 1
