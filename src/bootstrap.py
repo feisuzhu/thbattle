552f8adf57a7146d782e2e031fc73eaf884c1698
Proton <feisuzhu@163.com>
2020-10-25 17:17:44 +0800

    :construction: Bootstrap tunes

--- src/bootstrap.py
+++ src/bootstrap.py
@@ -15,24 +15,13 @@ if TYPE_CHECKING:
 
 
 # -- code --
-def unhandled_exception(etype, value, tb):
-    try:
-        import traceback
-        fn = os.path.join(workdir, 'exception.log')
-        with open(fn, 'w') as f:
-            f.write(traceback.format_exception(etype, value, tb))
-    except:  # noqa
-        pass
-
-
 def run_core() -> None:
     import logging
     import threading
 
     import utils.log
     import settings
-
-    workdir = os.environ['APPLICATION_WORKING_DIR']
+    import sys
 
     utils.log.init_embedded(os.path.join(workdir, 'client.log'), logging.INFO, settings.SENTRY_DSN, settings.VERSION)
 
