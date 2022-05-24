0a617b09bbafcbacc58f2f0a6ca52d6d84a87e53
Proton <feisuzhu@163.com>
2022-05-24 22:34:23 +0800

    feat: New Flandre

--- src/thb/characters/flandre.py
+++ src/thb/characters/flandre.py
@@ -4,68 +4,220 @@ from __future__ import absolute_import
 # -- stdlib --
 # -- third party --
 # -- own --
-from game.autoenv import EventHandler, Game
-from thb.actions import Damage, DrawCards, LaunchCard, PlayerDeath, PlayerTurn
-from thb.cards import AttackCard, DuelCard, Heal, Skill, UserAction, t_None
+from game.autoenv import EventHandler, Game, user_input
+from thb.actions import ActionStageLaunchCard, Damage, DrawCardStage, GenericAction, PlayerDeath, FinalizeStage
+from thb.actions import PlayerTurn, register_eh, ttags, PrepareStage, user_choose_cards, DropCards
+from thb.cards import ActionLimitExceeded, AttackCard, AttackCardVitalityHandler, BaseAttack
+from thb.cards import BaseDuel, DuelCard, ElementalReactorSkill, Skill, UserAction, t_None
 from thb.characters.baseclasses import Character, register_character_to
+from thb.inputlets import ChooseOptionInputlet
 
 
 # -- code --
-class ForbiddenFruits(Skill):
+class CriticalStrike(Skill):
     associated_action = None
-    skill_category = ('character', 'passive', 'compulsory')
+    skill_category = ('character', 'passive')
     target = t_None
 
 
-class ForbiddenFruitsHeal(Heal):
+class CriticalStrikeAction(GenericAction):
+    def apply_action(self):
+        return True
+
+
+class CriticalStrikeDropAction(GenericAction):
+    card_usage = 'drop'
+
+    def __init__(self, source, target):
+        self.source = source
+        self.target = target
+        self.cards = []
+
+    def apply_action(self):
+        tgt = self.target
+        if tgt.dead: return False
+
+        g = Game.getgame()
+        cards = user_choose_cards(self, tgt, ('cards', 'showncards', 'equips'))
+        if cards:
+            g.process_action(DropCards(tgt, tgt, cards=cards))
+        else:
+            from itertools import chain
+            cards = list(chain(tgt.cards, tgt.showncards, tgt.equips))[-1:]
+            g.players.reveal(cards)
+            g.process_action(DropCards(tgt, tgt, cards=cards))
+
+        self.cards = cards
+        return True
+
+    def cond(self, cards):
+        tgt = self.target
+        if not len(cards) == 1:
+            return False
+
+        if not all(c.resides_in in (tgt.cards, tgt.showncards, tgt.equips) for c in cards):
+            return False
+
+        from thb.cards import Skill
+        if any(c.is_card(Skill) for c in cards):
+            return False
+
+        return True
+
+
+class CriticalStrikeLimit(ActionLimitExceeded):
     pass
 
 
-class ForbiddenFruitsHandler(EventHandler):
-    interested = ('action_after',)
-    execute_after = ('DyingHandler',)
+class CriticalStrikeHandler(EventHandler):
+    interested = ('action_apply', 'action_before', 'action_after', 'action_shootdown', 'action_stage_action')
+    execute_after = (
+        'AttackCardHandler',
+        'FrozenFrogHandler',
+        'ElementalReactorHandler',
+        'ReversedScalesHandler',
+    )
     execute_before = (
-        'AyaRoundfanHandler',
-        'NenshaPhoneHandler',
-        'DilemmaHandler',
-        'DecayDamageHandler',
-        'EchoHandler',
-        'MelancholyHandler',
-        'MajestyHandler',
-        'MasochistHandler',
+        'MomijiShieldHandler',
+        'WineHandler',
     )
 
     def handle(self, evt_type, act):
-        if evt_type == 'action_after' and isinstance(act, Damage):
-            src = act.source
-            if act.cancelled: return act
-            if not src: return act
-            if not src.has_skill(ForbiddenFruits): return act
+        if evt_type == 'action_before' and isinstance(act, PrepareStage):
+            ttags(act.target)['flan_targets'] = []
 
+        elif evt_type == 'action_before' and isinstance(act, FinalizeStage):
+            if ttags(act.target)['flan_cs']:
+                g = Game.getgame()
+                tgt = act.target
+                g.process_action(CriticalStrikeDropAction(tgt, tgt))
+
+        elif evt_type == 'action_apply' and isinstance(act, BaseAttack):
+            src, tgt = act.source, act.target
+            if not self.in_critical_strike(src):
+                return act
+
+            if isinstance(act, BaseAttack):
+                g = Game.getgame()
+                g.process_action(CriticalStrikeAction(src, tgt))
+                ttags(src)['flan_targets'].append(tgt)
+                act.damage += 1
+
+        elif evt_type == 'action_before' and isinstance(act, Damage):
             g = Game.getgame()
-            for lc in reversed(g.action_stack):
-                if isinstance(lc, LaunchCard):
-                    break
-            else:
+            pact = g.action_stack[-1]
+            if not isinstance(pact, BaseDuel):
                 return act
 
-            c = lc.card
-            if not (c.is_card(AttackCard) or c.is_card(DuelCard)):
+            src, tgt = act.source, act.target
+
+            if not self.in_critical_strike(src):
                 return act
 
-            if lc.source is src:
-                n = act.amount * 2 - (src.maxlife - src.life)
-                if n > 0:
-                    g.process_action(DrawCards(src, n))
-                    src.maxlife - src.life and g.process_action(ForbiddenFruitsHeal(src, src, src.maxlife - src.life))
-                else:
-                    g.process_action(ForbiddenFruitsHeal(src, src, act.amount * 2))
+            g.process_action(CriticalStrikeAction(src, tgt))
+            act.amount += 1
+
+        elif evt_type == 'action_after' and isinstance(act, Damage):
+            src, tgt = act.source, act.target
+            if not self.in_critical_strike(src):
+                return act
+            ttags(src)['flan_cs'] = True
+
+        elif evt_type == 'action_before' and isinstance(act, ActionStageLaunchCard):
+            src = act.source
+            if not self.in_critical_strike(src):
+                return act
+
+            if act.card.is_card(AttackCard):
+                act.vitality_consumed = True
+                src.tags['vitality'] -= 1
+
+        elif evt_type == 'action_shootdown':
+            if not isinstance(act, ActionStageLaunchCard): return act
+            c = act.card
+            src = act.source
+            if not self.in_critical_strike(src): return act
+            if not c.is_card(AttackCard): return act
+            if src.has_skill(ElementalReactorSkill): return act
+            if src.tags['vitality'] > 0: return act
+            if getattr(act, 'vitality_consumed', False): return act
+            if set(act.target_list) & set(ttags(src)['flan_targets']):
+                raise CriticalStrikeLimit
+
+            return act
+
+        elif evt_type == 'action_stage_action':
+            tgt = act
+            if not self.in_critical_strike(tgt): return act
+            AttackCardVitalityHandler.disable(tgt)
 
         return act
 
+    def in_critical_strike(self, p):
+        return (
+            Game.getgame().current_player is p and
+            p.has_skill(CriticalStrike)
+        )
 
-# @register_character_to('common')
+
+class Exterminate(Skill):
+    associated_action = None
+    skill_category = ('character', 'passive')
+    target = t_None
+
+
+class ExterminateAction(UserAction):
+
+    def apply_action(self):
+        tgt = self.target
+        tgt.tags['exterminate'] = True
+        for s in tgt.skills:
+            if 'character' in s.skill_category:
+                tgt.disable_skill(s, 'exterminate')
+
+        return True
+
+
+class ExterminateHandler(EventHandler):
+    interested = ('choose_target',)
+
+    def handle(self, evt_type, arg):
+        if evt_type == 'choose_target':
+            act, tl = arg
+            src = act.source
+            g = Game.getgame()
+
+            if not src.has_skill(Exterminate):
+                return arg
+
+            c = act.card
+            if not c.is_card(AttackCard) and not c.is_card(DuelCard):
+                return arg
+
+            for tgt in tl:
+                g.process_action(ExterminateAction(src, tgt))
+
+        return arg
+
+
+@register_eh
+class ExterminateFadeHandler(EventHandler):
+    interested = ('action_after', 'action_apply')
+
+    def handle(self, evt_type, arg):
+        if ((evt_type == 'action_after' and isinstance(arg, PlayerTurn)) or
+            (evt_type == 'action_apply' and isinstance(arg, PlayerDeath) and arg.target.has_skill(Exterminate))):  # noqa
+
+            g = Game.getgame()
+            for p in g.players:
+                if p.tags.pop('exterminate', ''):
+                    p.reenable_skill('exterminate')
+
+        return arg
+
+
+@register_character_to('common')
 class Flandre(Character):
-    skills = [ForbiddenFruits]
-    eventhandlers_required = [ForbiddenFruitsHandler]
-    maxlife = 3
+    skills = [CriticalStrike, Exterminate]
+    eventhandlers_required = [CriticalStrikeHandler, ExterminateHandler]
+    maxlife = 4
--- src/thb/ui/ui_meta/characters/flandre.py
+++ src/thb/ui/ui_meta/characters/flandre.py
@@ -22,12 +22,62 @@ class Flandre:
     miss_sound_effect = u'thb-cv-flandre_miss'
 
 
-class ForbiddenFruits:
+class CriticalStrike:
     # Skill
-    name = u'禁果'
+    name = u'狂咲'
     description = (
-        u'|B锁定技|r，每当你使用|G弹幕|r或|G弹幕战|r造成伤害后，你回复伤害值两倍的体力，你摸以此方式回复的超出体力上限数值等量的牌。'
+        u'|B锁定技|r，你使用弹幕或弹幕战造成的伤害+1。你没有干劲时可以对出牌阶段内没有成为过弹幕目标的其他角色使用弹幕。回合结束时，如果你内造成过伤害，须弃置一张牌。'
     )
 
     clickable = passive_clickable
     is_action_valid = passive_is_action_valid
+
+
+class CriticalStrikeLimit:
+    target_independent = False
+    shootdown_message = u'你对这个角色使用过弹幕了'
+
+
+class CriticalStrikeAction:
+    def effect_string(act):
+        return u'|G【%s】|r 狂咲效果' % (
+            act.target.ui_meta.name,
+        )
+
+    def sound_effect(act):
+        return 'thb-cv-flandre_cs'
+
+
+class CriticalStrikeDropAction:
+    def effect_string(act):
+        return u'|G【%s】|r 狂咲弃牌效果' % (
+            act.target.ui_meta.name,
+        )
+
+    # choose_card
+    def choose_card_text(g, act, cards):
+        if act.cond(cards):
+            return (True, u'「狂咲」：弃置这张牌')
+        else:
+            return (False, u'「狂咲」：请弃置一张牌')
+
+
+class Exterminate:
+    # Skill
+    name = u'毁灭'
+    description = u'|B锁定技|r，每当你使用|G弹幕|r或|G弹幕战|r指定其他角色为目标后，其所有技能无效直到回合结束。'
+
+    clickable = passive_clickable
+    is_action_valid = passive_is_action_valid
+
+
+class ExterminateAction:
+    def effect_string(act):
+        return u'|G【%s】|r被|G【%s】|r玩坏了……' % (
+            act.target.ui_meta.name,
+            act.source.ui_meta.name,
+        )
+
+    def sound_effect(act):
+        return None
+        return 'thb-cv-flandre_cs'
